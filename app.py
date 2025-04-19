bc7248fd3ad8ae8c018efd50e52123cae8496b74
Hariprasath1911 <hari07011996@gmail.com>
2025-04-19 13:06:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,28 @@ from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceHub
 import os
 from dotenv import load_dotenv
+import base64
 st.set_page_config(page_title="Amazon Prime Chatbot", page_icon="🎬")
+def set_background_image_local(image_path):
+    with open(image_path, "rb") as file:
+        data = file.read()
+    base64_image = base64.b64encode(data).decode("utf-8")
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{base64_image}");
+            background-size: contain;
+            background-position: fit;
+            background-repeat: repeat;
+            background-attachment: fixed;
+        }}     
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background_image_local(r"amazon.png")
 # Load local .env (for development)
 load_dotenv()
 
